b70ae04a1f2afd86f57f340b24eede9473f3a3d9
chriskthomas <135461249+chriskthomas@users.noreply.github.com>
2023-06-03 15:49:21 -0500

    initial release

---
+++ README.md
@@ -0,0 +1,9 @@
+# author-child
+
+![Screenshot of the Author Child theme](screenshot.png)
+
+A child theme of [Author by Compete Themes](https://github.com/BenSibley/Author). This will not work without the [Font Awesome Wordpress Plugin](https://wordpress.org/plugins/font-awesome/) or some other way to load the Font Awesome icons.
+
+## SASS Folder
+
+I do not intend to edit any sass files except for the `style.scss` file and the `_variables.scss` file. Other customizations will be placed in the `_child_theme.scss` file.
---
+++ functions.php
@@ -0,0 +1,47 @@
+<?php
+
+/*
+// Load main css for parent and child theme (only needed if we don't want to build a new style.css file)
+add_action('wp_enqueue_scripts', 'my_theme_enqueue_styles');
+function my_theme_enqueue_styles()
+{
+    $parenthandle = 'ct-author-style'; // This is 'twentyfifteen-style' for the Twenty Fifteen theme.
+    $theme = wp_get_theme();
+    // Parent theme css
+    wp_enqueue_style($parenthandle,
+        get_template_directory_uri() . '/style.min.css',
+        array(), // If the parent theme code has a dependency, copy it to here.
+        $theme->parent()->get('Version')
+    );
+    // Child theme css
+    wp_enqueue_style('child-style',
+        get_stylesheet_uri(),
+        array($parenthandle),
+        $theme->get('Version') // This only works if you have Version defined in the style header.
+    );
+}
+*/
+
+// Remove fonts from parent theme
+add_action('wp_enqueue_scripts', 'mwt_remove_parent_styles', 20);
+function mwt_remove_parent_styles()
+{
+    // Google fonts
+    wp_dequeue_style('ct-author-google-fonts');
+    // Font Awesome
+    wp_dequeue_style('ct-author-font-awesome');
+}
+
+// Customize footer
+function author_footer_callback()
+{
+    return empty(get_bloginfo('description')) ? get_bloginfo('name') : get_bloginfo('name') . " | " . get_bloginfo('description');
+}
+add_filter('ct_author_footer_text', 'author_footer_callback');
+
+//Remove Gutenberg Block Library CSS from loading on the frontend
+function remove_wp_block_library_css(){
+    wp_dequeue_style( 'wp-block-library' );
+    wp_dequeue_style( 'wp-block-library-theme' );
+} 
+add_action( 'wp_enqueue_scripts', 'remove_wp_block_library_css', 100 );

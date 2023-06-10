969955d78774eb125595b4e5b0074169f61a3369
Chris K. Thomas <135461249+chriskthomas@users.noreply.github.com>
2023-06-10 04:16:53 +0000

    generate fontawesome subset

---
+++ build.mjs
@@ -0,0 +1,10 @@
+// Import fontawesome-subset
+import { fontawesomeSubset } from "fontawesome-subset";
+
+fontawesomeSubset(
+  {
+    solid: ["bars", "envelope"],
+    brands: ["twitter-square", "linkedin", "github-square", "mastodon"],
+  },
+  "assets/font-awesome/webfonts/"
+);

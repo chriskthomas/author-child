9d780d31cc016311b7b62b1ffbdaf0d4a8d900d2
Chris K. Thomas <135461249+chriskthomas@users.noreply.github.com>
2023-07-05 02:25:33 +0000

    add phone icon

--- build.js
+++ build.js
@@ -4,7 +4,7 @@ const fontawesomeSubset = require("fontawesome-subset").fontawesomeSubset;
 
 fontawesomeSubset(
   {
-    solid: ["bars", "envelope"],
+    solid: ["bars", "envelope", "phone"],
     brands: ["twitter-square", "linkedin", "github-square", "mastodon"],
   },
   "assets/fonts/"
